5a1d8ef4b04d4ba7ca28b47b782bc102d5da0ccc
unknown <ibrahim.neo98@gmail.com>
2023-02-07 16:48:38 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <table className='t'>
+   <tr>
+    <th>Mobile</th>
+    <th>Model</th>
+    <th>Price</th>
+  </tr>
+  <tr>
+    <td>Apple</td>
+    <td>iphone-10</td>
+    <td>100000</td>
+  </tr>
+  <tr>
+    <td>Samsung</td>
+    <td>s12+pro </td>
+    <td>80000</td>
+  </tr>
+   </table>
+   
+     </div>
+  );
+}
+
+export default App;
